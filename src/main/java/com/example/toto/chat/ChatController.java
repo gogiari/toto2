d52b7ef074f782e0d93189f82e4b754c65d8277e
lck9677 <lck9677@icloud.com>
2023-05-23 11:15:10 +0900

    chat --ck

---
+++ src/main/java/com/example/toto/chat/ChatController.java
@@ -0,0 +1,26 @@
+package com.example.toto.chat;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ChatController {
+
+    @GetMapping("/chat")
+    public String index(){
+        return "error";
+    }
+
+    @GetMapping("/chat/{id}")
+    public String chattingRoom(@PathVariable String id, HttpSession session, Model model){
+           
+        model.addAttribute("name",id);
+        
+        
+        return "chattingRoom2";
+    }
+}
